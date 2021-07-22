a8d55346ecf3df2541bb3381ff1630bc18f851aa
Depressed Foundation <depressedfoundation@outlook.com>
2021-07-23 03:08:34 +0800

    Update bear.tsx

--- src/configs/bear.tsx
+++ src/configs/bear.tsx
@@ -59,6 +59,105 @@ const bear: BearData[] = [
         icon: <HiFire />,
         excerpt: "A deep learning framework implemented in Numpy...",
         link: "https://github.com/Renovamen/flint"
+      },
+      {
+        id: "metallic",
+        title: "Metallic",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/metallic/master/README.md",
+        icon: <GiJumpingDog />,
+        excerpt: "A meta-learning library base on PyTorch...",
+        link: "https://github.com/Renovamen/metallic"
+      },
+      {
+        id: "alkaid",
+        title: "Alkaid",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/alkaid/main/README.md",
+        icon: <GiCat />,
+        excerpt: "A reinforcement-learning toolbox for PyTorch...",
+        link: "https://github.com/Renovamen/alkaid"
+      },
+      {
+        id: "portfolio-macos",
+        title: "Portfolio macOS",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/playground-macos/main/README.md",
+        icon: <RiGamepadLine />,
+        excerpt: "My portfolio website simulating macOS's GUI...",
+        link: "https://github.com/Renovamen/playground-macos"
+      },
+      {
+        id: "oh-vue-icons",
+        title: "Oh, Vue Icons!",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/oh-vue-icons/master/README.md",
+        icon: <FaVuejs />,
+        excerpt: "Importing icons from different icon packs in Vue easily...",
+        link: "https://oh-vue-icons.netlify.app"
+      },
+      {
+        id: "gungnir",
+        title: "Gungnir",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/vuepress-theme-gungnir/main/README.md",
+        icon: <GiEnergySword />,
+        excerpt: "A simple and beautiful blog theme for VuePress...",
+        link: "https://vuepress-theme-gungnir.vercel.app"
+      },
+      {
+        id: "text-classification",
+        title: "Text Classification",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/Text-Classification/master/README.md",
+        icon: <CgFormatText />,
+        excerpt: "PyTorch implementation of text classificaiton models...",
+        link: "https://github.com/Renovamen/Text-Classification"
+      },
+      {
+        id: "speech-emotion-recognition",
+        title: "Speech Emotion",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/Speech-Emotion-Recognition/master/README.md",
+        icon: <AiFillAudio />,
+        excerpt: "Speech emotion recognition using Keras and sklearn...",
+        link: "https://github.com/Renovamen/Speech-Emotion-Recognition"
+      },
+      {
+        id: "image-captioning",
+        title: "Image Captioning",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/Image-Captioning/master/README.md",
+        icon: <BsFillImageFill />,
+        excerpt: "PyTorch implementation of image captioning models...",
+        link: "https://github.com/Renovamen/Image-Captioning"
+      },
+      {
+        id: "cube-solver",
+        title: "Cube Solver",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/Just-a-Cube/master/README.md",
+        icon: <BiCube />,
+        excerpt: "A rubik's cube solver (layer-by-layer & two-phase)...",
+        link: "https://cube.zxh.io"
+      },
+      {
+        id: "midgard",
+        title: "Interactable Resume",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/midgard/master/README.md",
+        icon: <BiGame />,
+        excerpt: "An interactable version of my resume, powered by Vue...",
+        link: "https://resume.zxh.io/"
+      },
+      {
+        id: "fishmail",
+        title: "Fishmail",
+        file:
+          "https://raw.githubusercontent.com/Renovamen/Fishmail/master/README.md",
+        icon: <GiFishEscape />,
+        excerpt: "上班的时候装作在 Gmail 上查邮件的样子看知乎摸鱼...",
+        link: "https://fishmail.vercel.app"
       }
     ]
   }
