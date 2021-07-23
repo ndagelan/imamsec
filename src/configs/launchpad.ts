797dc403746fec97d5b9b614883464ce85a45b19
Depressed Foundation <depressedfoundation@outlook.com>
2021-07-23 04:48:01 -0700

    Update launchpad.ts

--- src/configs/launchpad.ts
+++ src/configs/launchpad.ts
@@ -1,54 +1,5 @@
 import { LaunchpadData } from "../types";
 
-const launchpadApps: LaunchpadData[] = [
-  {
-    id: "flint",
-    title: "Flint",
-    img: "img/icons/launchpad/flint.png",
-    link: "https://github.com/Renovamen/flint"
-  },
-  {
-    id: "metallic",
-    title: "Metallic",
-    img: "img/icons/launchpad/meta.png",
-    link: "https://github.com/Renovamen/metallic"
-  },
-  {
-    id: "alkaid",
-    title: "Alkaid",
-    img: "img/icons/launchpad/rl.png",
-    link: "https://github.com/Renovamen/alkaid"
-  },
-  {
-    id: "oh-vue-icons",
-    title: "Oh, Vue Icons!",
-    img: "img/icons/launchpad/icon.png",
-    link: "https://oh-vue-icons.netlify.app/"
-  },
-  {
-    id: "gungnir",
-    title: "Gungnir",
-    img: "img/icons/launchpad/gungnir.png",
-    link: "https://vuepress-theme-gungnir.vercel.app/"
-  },
-  {
-    id: "what-if",
-    title: "My Notebook",
-    img: "img/icons/launchpad/notebook.png",
-    link: "https://note.zxh.io"
-  },
-  {
-    id: "cube",
-    title: "Cube Solver",
-    img: "img/icons/launchpad/cube.png",
-    link: "https://cube.zxh.io/"
-  },
-  {
-    id: "fishmail",
-    title: "Fishmail",
-    img: "img/icons/launchpad/fishmail.png",
-    link: "https://fishmail.vercel.app/"
-  }
-];
+const launchpadApps: LaunchpadData[] = [];
 
 export default launchpadApps;
