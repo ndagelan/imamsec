38639ef7d55f3f8b8d417cb9408048f5dc5eed0c
Depressed Foundation <depressedfoundation@outlook.com>
2021-07-25 22:18:48 +0700

    ds

--- src/configs/bear.tsx
+++ src/configs/bear.tsx
@@ -35,7 +35,7 @@ const bear: BearData[] = [
       {
         id: "about-galery",
         title: "Galery",
-        file: "markdown/about-site.md",
+        file: "markdown/galery.md",
         icon: <GoBrowser />,
         excerpt: "About galery of this personal portfolio site..."
       }
